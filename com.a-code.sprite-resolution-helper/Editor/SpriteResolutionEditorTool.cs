d81394cc48dd3cb9e7b58e3ca6d65887ff7b32a2
Ars <arseniy59@yandex.ru>
2020-10-08 16:05:45 +0300

    Copy project to git

---
+++ com.a-code.sprite-resolution-helper/Editor/SpriteResolutionEditorTool.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SpriteResolutionHelper
+{
+    public static class SpriteResolutionEditorTool
+    {
+        private const string IsUsedMenuPath = "Tools/SpriteResolutionHelper/IsUsed";
+        private const string IsUsedMenuItemName = "IsUsed";
+
+        public static bool IsUsed
+        {
+            get => EditorPrefs.GetBool(IsUsedMenuItemName, true);
+            set => EditorPrefs.SetBool(IsUsedMenuItemName, value);
+        }
+
+        [MenuItem(IsUsedMenuPath)]
+        private static void IsUsedSpriteResolutionHelper()
+        {
+            IsUsed = !IsUsed;
+        }
+
+        [MenuItem(IsUsedMenuPath, true)]
+        private static bool IsUsedSpriteResolutionHelperValidate()
+        {
+            Menu.SetChecked(IsUsedMenuPath, IsUsed);
+            return true;
+        }
+    }
+}
---
+++ com.a-code.sprite-resolution-helper/Editor/SpriteResolutionHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace SpriteResolutionHelper
+{
+    public class SpriteResolutionHelper : UnityEditor.AssetModificationProcessor
+    {
+        private const string SpriteFileType = ".png";
+
+        static void OnWillCreateAsset(string assetName)
+        {
+            if (!SpriteResolutionEditorTool.IsUsed || !assetName.Contains(SpriteFileType))
+                return;
+
+            var assetFullPath = GetAssetFullPath(assetName);
+            var texture = TextureFromFile(assetFullPath);
+
+            if (texture.width % 4 != 0 || texture.height % 4 != 0)
+                Debug.LogWarning($"{assetName} have incorrect resolution");
+        }
+
+        private static string GetAssetFullPath(string assetName)
+        {
+            var metaStartIndex = assetName.IndexOf(".meta");
+            var assetPath = assetName.Remove(metaStartIndex);
+
+            return Path.Combine(Path.GetDirectoryName(Application.dataPath), assetPath);
+        }
+
+        private static Texture TextureFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException(filePath);
+
+            var bytes = File.ReadAllBytes(filePath);
+            var texture = new Texture2D(1, 1);
+            texture.LoadImage(bytes);
+
+            return texture;
+        }
+    }
+}
